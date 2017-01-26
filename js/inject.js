bbb4e0e3f0bcac93d4f0fb6ee8caf7832c8c8fb4
DarkAce65 <tvdood99@gmail.com>
2017-01-26 14:23:45 -0500

    Convert indentation to spaces

--- js/inject.js
+++ js/inject.js
@@ -245,34 +245,34 @@ function modernTheme() {
 
     /* Fancy select boxes */
     $('select:not([multiple])').each(function() {
-		var sb = this;
+        var sb = this;
         $(sb).wrap('<div class="select"></div>');
-		var opts = sb.options;
-		var $current = $('<div class="current">' + opts[opts.selectedIndex].innerHTML.trim() + '</div>');
-		$(sb).before($current);
-		var optionsDiv = '<div class="options">';
-		for(var i = 0; i < opts.length; i++) {
-			optionsDiv += '<div class="option" data-value="' + opts[i].value + '">' + opts[i].innerHTML.trim() + '</div>';
-		}
-		var $options = $(optionsDiv + '</div>');
-		$(sb).after($options);
+        var opts = sb.options;
+        var $current = $('<div class="current">' + opts[opts.selectedIndex].innerHTML.trim() + '</div>');
+        $(sb).before($current);
+        var optionsDiv = '<div class="options">';
+        for(var i = 0; i < opts.length; i++) {
+            optionsDiv += '<div class="option" data-value="' + opts[i].value + '">' + opts[i].innerHTML.trim() + '</div>';
+        }
+        var $options = $(optionsDiv + '</div>');
+        $(sb).after($options);
 
-		$current.click(function(e) {
-			e.stopPropagation();
-			var $select = $(sb).parent(".select");
-			$select.toggleClass("active");
-			$(".select").not($select).removeClass("active");
-		});
+        $current.click(function(e) {
+            e.stopPropagation();
+            var $select = $(sb).parent(".select");
+            $select.toggleClass("active");
+            $(".select").not($select).removeClass("active");
+        });
 
-		$options.find(".option").click(function(e) {
-			e.stopPropagation();
-			$current.html(this.innerHTML);
-			sb.value = $(this).data("value");
-			$(sb).parent(".select").removeClass("active");
-		});
+        $options.find(".option").click(function(e) {
+            e.stopPropagation();
+            $current.html(this.innerHTML);
+            sb.value = $(this).data("value");
+            $(sb).parent(".select").removeClass("active");
+        });
     });
 
-	$(document).click(function() {
-		$(".select").removeClass("active");
-	});
+    $(document).click(function() {
+        $(".select").removeClass("active");
+    });
 }
